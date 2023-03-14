7caf0d582da9db45c45defd85561fe48e192641d
carlostobarra <127849253+carlostobarra@users.noreply.github.com>
2023-03-14 12:31:55 +0100

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-streamli.text('Some emojis: 🥣 🥗 🐔 🥑🍞')
+streamlit.text('Some emojis: 🥣 🥗 🐔 🥑🍞')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
